0630599c76b7a61f3ed4400f9c86e8ac69ffc35e
petksand <fbsocial@yahoo.com>
2019-11-15 03:08:33 -0500

    Adding comments to code

--- baseline.py
+++ baseline.py
@@ -1,27 +1,29 @@
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
+import os
 import pickle
 from collections import Counter
-from skimage.color import rgb2lab, deltaE_cie76
-import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 from skimage import io
+from skimage.color import deltaE_cie76, rgb2lab
+from sklearn.cluster import KMeans
 
 
-# load test image
 def load_img(filepath):
-    if not filepath:
-        raise Exception
+    """
+    Loads image
+    """
     img = cv2.imread(filepath)
     # convert to proper RGB scale
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
     return img
 
-def RGB2HEX(color):
-    return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
-
 def get_colours(img, num_colours, show_chart=False):
+    """
+    Get top n = num_colours colours from a given img
+    """
     # resize to make processing time shorter (and it doesn't affect colours)
     mod_img = cv2.resize(img, (400, 400), interpolation = cv2.INTER_AREA)
     # reshape
@@ -34,7 +36,6 @@ def get_colours(img, num_colours, show_chart=False):
     center_colors = clf.cluster_centers_
     # We get ordered colors by iterating through the keys
     ordered_colors = [center_colors[i] for i in counts.keys()]
-    # hex_colors = [RGB2HEX(ordered_colors[i]) for i in counts.keys()]
     rgb_colors = [list(ordered_colors[i]) for i in counts.keys()]
 
     if (show_chart):
@@ -43,15 +44,21 @@ def get_colours(img, num_colours, show_chart=False):
         plt.show()
 
     return rgb_colors
-    # return [rgb_colors, counts.values()]
 
 def get_files(path):
+    """
+    Gets all files from a given path
+    """
     files = os.listdir(path)
     if ".DS_Store" in files:
         files.remove(".DS_Store")
+    
     return files
 
 def get_avg_colour(avg, n):
+    """
+    Calculates average colour
+    """
     # get colours out
     red = avg[:,:,0]
     green = avg[:,:,1]
@@ -67,6 +74,9 @@ def get_avg_colour(avg, n):
     return final
 
 def save_list(path, lst):
+    """
+    Saves list as a pickle file
+    """
     output = open("{}.pkl".format(path), "wb")
     pickle.dump(avg, output)
     output.close()
@@ -74,15 +84,14 @@ def save_list(path, lst):
     return True
 
 if __name__ == "__main__":
+    # get all eras in the dataset
     path = "artist_dataset/"
     eras = get_files(path)
     for era in eras:
-        if era != "Post_Impressionism":
-            continue
         artists = get_files("{}{}/".format(path,era))
+        # iterate through all artists in era
         for artist in artists:
-            if artist == "emily_carr" or artist == "paul_cezanne":
-                continue
+            # get all artworks from the artist
             pics = get_files("{}{}/{}/".format(path,era,artist))
             num_pics = len(pics)
             avg = []
@@ -90,15 +99,16 @@ if __name__ == "__main__":
                 pic_path = "{}{}/{}/{}".format(path, era, artist,pic)
                 # print progress
                 print("Era: {} Artist: {} {}/{}".format(era, artist, i, num_pics))
-                print(pic_path)
+                # load image + get top 8 colours
                 img = load_img(pic_path)
                 avg.append(get_colours(img,8,False))
             avg = np.array(avg)
+            # ger overall average colours
             final = get_avg_colour(avg, num_pics)
+            # save as pickle files
             save_list("averages/{}_{}".format(era,artist), avg)
             save_list("final/{}_{}".format(era,artist), final)
-            plt.imshow(final)
+            # display
+            # plt.imshow(final)
+            # save figure
             plt.savefig('final/colours_{}_{}.png'.format(era,artist), bbox_inches='tight')
-
-    # plt.show()
-    # print(final)
