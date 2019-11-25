c13bb6ade97d43f5b1c9eda013b6650dd183a143
petksand <fbsocial@yahoo.com>
2019-11-25 04:41:20 -0500

    Fixed my stupidity. Proper evaluation from era to artist and confusion matrix code

--- artist_bot.py
+++ artist_bot.py
@@ -13,7 +13,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.utils.multiclass import unique_labels
 from torchtext import data
 from torchvision import datasets, models
-from torchvision import transforms as transforms
 
 from art_model import ArtNet
 
@@ -30,59 +29,6 @@ _ARTISTS_ = [5, 4, 3, 0, 4, 4, 0, 0, 2, 2, 1, 5, 4, 3, 2, 1, 5, 1, 1, 2, 5, 3, 0
 
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
-
-def plot_confusion_matrix(y_true, y_pred, classes,
-                          normalize=False,
-                          title=None,
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if not title:
-        if normalize:
-            title = 'Normalized confusion matrix'
-        else:
-            title = 'Confusion matrix, without normalization'
-
-    # Compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-    # Only use the labels that appear in the data
-    classes = classes[unique_labels(y_true, y_pred)]
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-    ax.figure.colorbar(im, ax=ax)
-    # We want to show all ticks...
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
-           xticklabels=classes, yticklabels=classes,
-           title=title,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
-    fig.tight_layout()
-    return ax
 
 def get_model(filepath, art=True, transfer=True):
     if not transfer:
@@ -146,7 +92,6 @@ for obj in artist_testset:
     # check era accuracy
     if int(era_pred) == int(era):
         corr_era += 1
-    update_conf(era, era_pred, conf=0)
 
     # input into art
     art_preds = ArtNet(img)
@@ -155,16 +100,15 @@ for obj in artist_testset:
 
     # evaluate without era
     art_pred = art_preds.index(max(art_preds))
-    if int(artist) == int(art_pred):
+    if float(artist) == float(art_pred):
         corr_art += 1
-    update_conf(artist, art_pred, conf=1)
 
     # take out indexes only for appropriate era
     art_preds_cut = [art_preds[k] for k in _ERAS_[era]]
     art_pred_from_era = art_preds_cut.index(max(art_preds_cut))
-    if int(artist) == int(art_pred_from_era):
+    art_pred_from_era = _ERAS_[era][art_pred_from_era]
+    if float(artist) == float(art_pred_from_era):
         corr_era_to_art += 1
-    update_conf(artist, art_pred_from_era, conf=2)
 
     pred_era.append(era_pred)
     pred_artist.append(art_pred)
@@ -183,9 +127,3 @@ np.savetxt("true_artist.txt", np.asarray(true_artist))
 np.savetxt("pred_era.txt", np.asarray(pred_era))
 np.savetxt("pred_artist.txt", np.asarray(pred_artist))
 np.savetxt("pred_artist_from_era.txt", np.asarray(pred_artist_from_era))
-
-# create confusion matrix
-# plot_confusion_matrix(true_era, pred_era, classes=["Baroque", "Cubism", "Impressionism", "Pop Art", "Realism", "Renaissance"], title="Era Confusion Matrix")
-# plot_confusion_matrix(true_artist, pred_artist, classes=artist_testset.classes, title="Artist Confusion Matrix")
-# plot_confusion_matrix(true_artist, pred_artist_from_era, classes=artist_testset.classes, title="Artist from Era Confusion Matrix")
-# plt.show()
